c44f1b1f4e883b9d25bf2ffa332bd8dd72ef7ae2
sinchanac22 <143880087+sinchanac22@users.noreply.github.com>
2025-04-05 00:08:35 +0530

    Update main.py

--- api_server/main.py
+++ api_server/main.py
@@ -1,4 +1,23 @@
+# main.py
+from fastapi import FastAPI, Request
+from api_server.node_manager import NodeManager
 from api_server.utils import launch_node_container
+
+app = FastAPI()
+node_manager = NodeManager()
+
+@app.get("/")
+def root():
+    return {"message": "API Server Running"}
+
+@app.post("/add_node/")
+def add_node(cpu_cores: int):
+    node_id = node_manager.register_node(cpu_cores)
+    return {"message": "Node added", "node_id": node_id}
+
+@app.get("/nodes/")
+def get_all_nodes():
+    return node_manager.get_nodes()
 
 @app.post("/add_node/")
 def add_node(cpu_cores: int):
